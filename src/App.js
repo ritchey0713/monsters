13575758f6436f1900cc3667b4b4d483c3744375
Dj <ritchey0713@gmail.com>
2020-03-16 13:08:22 -0400

    fetch generic names

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstein",
-          id: "123"
-        },
-        {
-          name: "Dracula",
-          id: "122"
-        },
-        {
-          name: "Zombie",
-          id: 1555
-        }
-      ]
+      monsters: []
     }
   }
+
+  componentDidMount = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(resp => resp.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <div>
             {
               this.state.monsters.map(monster =>(
